68595718bdfd98e43cffdc5b5b13d0ead10c4b60
Nazeeff <muhammednazeef6@gmail.com>
2023-01-22 20:24:11 +0530

    added titles for social handles

--- src/components/home/SocialHandles.tsx
+++ src/components/home/SocialHandles.tsx
@@ -7,12 +7,12 @@ const SocialHandles = () => {
   return (
     <div className="social-handles text-white mx-auto">
       <ul className="text-gray-500 flex items-center justify-center space-x-3">
-        <li className="twitter">
+        <li className="twitter" title="twitter.com/iamnazeef_">
           <a href="https://twitter.com/iamnazeef_" target="_block">
             <Twitter />
           </a>
         </li>
-        <li className="linkedin">
+        <li className="linkedin" title="linkedin.com/nazeef-muhammed">
           <a
             href="https://www.linkedin.com/in/nazeef-muhammed/"
             target="_block"
@@ -20,12 +20,12 @@ const SocialHandles = () => {
             <LinkedIn />
           </a>
         </li>
-        <li className="github">
+        <li className="github" title="github.com/iamnazeef">
           <a href="https://github.com/iamnazeef" target="_block">
             <GitHub />
           </a>
         </li>
-        <li className="instagram">
+        <li className="instagram" title="instagram.com/iamnazeef">
           <a href="https://www.instagram.com/iamnazeef/" target="_block">
             <Instagram />
           </a>
