405dd89ab59273be2507e565cea216f46fd2179a
Nazeef <muhammednazeef6@gmail.com>
2024-02-05 23:19:36 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,7 +12,7 @@ const Home: React.FC = () => {
           Nazeef Muhammed
           <br />
           <span className="italic font-[400] font-inter text-lg text-standardGray">
-            JavaScript Developer
+            Front-end engineer
           </span>
         </div>
       </header>
