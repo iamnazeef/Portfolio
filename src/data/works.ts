90aef34283838c30eace1021595d46303abc8fe3
iamnazeef <muhammednazeef6@gmail.com>
2024-05-18 18:48:23 +0530

    Update works.ts

--- src/data/works.ts
+++ src/data/works.ts
@@ -5,12 +5,5 @@ export const works = [
     startYear: '07/2023',
     endYear: 'Present',
     position: 'Jr. Product Engineer: Frontend'
-  },
-  {
-    organization: 'Global Infonet',
-    organizationWebsite: 'https://global-infonet.com/',
-    startYear: '07/2023',
-    endYear: '07/2023',
-    position: 'Software Engineer Intern'
-  },
+  }
 ]
